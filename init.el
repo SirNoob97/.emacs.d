90d39af2de3338026bb719863ed0b8d64ade5361
SirNoob97 <martinald467@gmail.com>
2021-09-14 21:19:13 +0200

    preparing for modules

--- init.el
+++ init.el
@@ -4,6 +4,35 @@
 ;;; Load the full configuration, divided in multiple files
 
 ;;; Code:
+
+;(setq debug-on-error t)
+
+(setq auto-mode-case-fold nil)
+
+;; don't GC during startup
+(setq gc-cons-percentage 0.6
+      gc-cons-threshold most-positive-fixnum)
+(add-hook 'emacs-startup-hook
+	  (lambda ()
+	    (setq gc-cons-threshold 800000
+		  gc-cons-percentage 0.1)))
+
+;; Add lisp to load-path
+(defun update-load-path (&rest _)
+  "Update `load-path'."
+  (dolist (dir '("lisp" "site-lisp"))
+    (push (expand-file-name dir user-emacs-directory) load-path)))
+
+(defun add-subdirs-to-load-path (&rest _)
+  "Add subdirectories to `load-path'."
+  (let ((default-directory (expand-file-name "site-lisp" user-emacs-directory)))
+    (normal-top-level-add-subdirs-to-load-path)))
+
+(advice-add #'package-initialize :after #'update-load-path)
+(advice-add #'package-initialize :after #'add-subdirs-to-load-path)
+
+(update-load-path)
+
 (set-language-environment "UTF-8")
 (set-default-coding-systems 'utf-8)
 (show-paren-mode 1)
