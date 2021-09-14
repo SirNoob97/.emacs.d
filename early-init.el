26bb0cd08163ed7b698ecf13c8c055de7f58430f
SirNoob97 <martinald467@gmail.com>
2021-09-14 21:16:05 +0200

    add early config to optimize startup

---
+++ early-init.el
@@ -0,0 +1,17 @@
+;;; early-init.el --- Load use-package -*- lexical-binding: t -*-
+
+;;; Commentary:
+;;; Load use-package before init.e
+
+;;; Code:
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
+
+(setq package-enable-at-startup nil)
+
+(setq frame-inhibit-implied-resize t)
+
+(push '(vertical-scroll-bars) default-frame-alist)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+;;; early-init.el ends here
