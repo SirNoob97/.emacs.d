7f7449c776042bda032d0febcdc33828791f31c6
SirNoob97 <martinald467@gmail.com>
2021-09-16 18:10:38 +0200

    formate

--- early-init.el
+++ early-init.el
@@ -14,4 +14,7 @@
 (push '(vertical-scroll-bars) default-frame-alist)
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
+
+(provide 'early-init)
+
 ;;; early-init.el ends here
