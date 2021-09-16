3b0bcecc074d982c492c50317d9352d98e0363c2
SirNoob97 <martinald467@gmail.com>
2021-09-16 18:11:58 +0200

    no needed

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -1,3 +1,10 @@
+;;; init-evil.el --- Load vim mode -*- lexical-binding: t -*-
+
+;;; Commentary:
+;;; Load vim mode
+
+;;; Code:
+
 (use-package evil
   :init
   (setq evil-want-integration t)
@@ -5,14 +12,8 @@
   (setq evil-want-C-u-scroll t)
   (setq evil-want-C-i-jump nil)
   :config
-  (evil-mode 1)
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
 
-  ;; Use visual line motions even outside of visual-line-mode buffers
-  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
-
-  (evil-set-initial-state 'messages-buffer-mode 'normal)
-  (evil-set-initial-state 'dashboard-mode 'normal))
-
 (provide 'init-evil)
+
+;;; init-evil.el ends here
