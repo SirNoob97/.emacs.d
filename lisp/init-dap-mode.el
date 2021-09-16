54c219b4f2811ef5f8d52bbe6b13e6f2ef126e2e
SirNoob97 <martinald467@gmail.com>
2021-09-16 21:21:19 +0200

    add key bindings

--- lisp/init-dap-mode.el
+++ lisp/init-dap-mode.el
@@ -8,10 +8,19 @@
 (use-package dap-mode
   :after lsp-mode
   :bind (:map lsp-mode-map
-            ("<f5>" . dap-debug)
-            ("M-<f5>" . dap-hydra))
+	      ("<f2>" . dap-debug)
+              ("M-<f2>" . dap-hydra)
+	      ("C-c l d b" . dap-ui-breakpoints)
+	      :map dap-mode-map
+	      ("<f5>" . dap-next)
+	      ("<f6>" . dap-step-in)
+	      ("<f7>" . dap-step-out)
+	      ("<f8>" . dap-continue)
+	      ("<f9>" . dap-breakpoint-toggle)
+	      )
   :config
   (dap-mode t)
+  ;; following only works on gui
   (dap-ui-mode t)
   (dap-tooltip-mode 1)
   (tooltip-mode 1))
