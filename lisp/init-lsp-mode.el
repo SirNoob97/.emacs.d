d87b00fa144743d904f04d5797336840820229ff
SirNoob97 <martinald467@gmail.com>
2021-09-17 19:32:34 +0200

    refactor, add performance and keymappings

--- lisp/init-lsp-mode.el
+++ lisp/init-lsp-mode.el
@@ -8,19 +8,27 @@
 (use-package lsp-mode
   :ensure t
   :defer 0
+  :bind
+  (:map lsp-mode-map
+	("M-RET" . lsp-execute-code-action)
+	("C-c l f b" . lsp-format-buffer)
+	("C-c l f r" . lsp-format-region))
   :hook (
    (lsp-mode . lsp-enable-which-key-integration)
    (java-mode . #'lsp-deferred))
   :init
-  (setq
-    lsp-keymap-prefix "C-c l" ; this is for which-key integration documentation, need to use lsp-mode-map
-    lsp-enable-file-watchers nil
-    read-process-output-max (* 1024 1024)  ; 1 mb
-    lsp-idle-delay 0.500)
+  (setq lsp-keymap-prefix "C-c l") ; this is for which-key integration documentation, need to use lsp-mode-map
+  (setq lsp-enable-file-watchers nil)
   (setq lsp-headerline-breadcrumb-enable nil)
-  :config
+  (setq lsp-lens-enable nil)
+  (setq lsp-eldoc-enable-hover nil)
   (setq lsp-enable-on-type-formatting nil)
+  (setq lsp-modeline-diagnostics-enable nil) ; flycheck handle this
+  (setq lsp-log-io nil)
+  (setq read-process-output-max (* 1024 1024))  ; 1 mb
+  (setq lsp-idle-delay 0.500)
   (setq gc-cons-threshold 100000000)
+  :config
   (define-key lsp-mode-map (kbd "C-c l") lsp-command-map))
 
 (provide 'init-lsp-mode)
