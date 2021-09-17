dc54af15170bf77545c4c219528c90f82ef27030
SirNoob97 <martinald467@gmail.com>
2021-09-17 19:35:09 +0200

    refactor

--- lisp/init-lsp-ui.el
+++ lisp/init-lsp-ui.el
@@ -13,6 +13,15 @@
          ([remap xref-find-references] . lsp-ui-peek-find-references))
   :init
   (setq lsp-ui-peek-always-show t)
+  (setq lsp-ui-doc-delay 1.5)
+  (setq	lsp-ui-doc-include-signature t)
+  (setq	lsp-ui-doc-position 'bottom)
+  (setq	lsp-ui-doc-max-width 100)
+  (setq lsp-ui-sideline-ignore-duplicate t)
+  (setq lsp-ui-sideline-show-diagnostics t)
+  (setq	lsp-ui-doc-show-with-cursor nil)
+  (setq	lsp-ui-doc-show-with-mouse nil)
+  (setq lsp-ui-sideline-show-code-actions nil)
   :config
   (define-key lsp-ui-mode-map (kbd "C-c l k") 'lsp-ui-doc-show)
   (define-key lsp-ui-mode-map (kbd "C-c l s") 'lsp-ui-doc-hide))
