47ff1bed0722f53821c29c27b9df3f50eedfd32c
SirNoob97 <martinald467@gmail.com>
2021-09-17 19:34:41 +0200

    fix loading issue

--- lisp/init-lsp-ui.el
+++ lisp/init-lsp-ui.el
@@ -8,20 +8,11 @@
 (use-package lsp-ui
   :ensure t
   :defer 0
-  :requires lsp-mode flycheck
   :bind (:map lsp-ui-mode-map
          ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
          ([remap xref-find-references] . lsp-ui-peek-find-references))
   :init
   (setq lsp-ui-peek-always-show t)
-  (setq lsp-ui-doc-delay 1.5
-	lsp-ui-doc-show-with-cursor nil
-	lsp-ui-doc-show-with-mouse nil
-	lsp-ui-doc-include-signature t
-	lsp-ui-doc-position 'bottom
-	lsp-ui-doc-max-width 100)
-  (setq lsp-ui-sideline-ignore-duplicate t
-	lsp-ui-sideline-show-diagnostics t)
   :config
   (define-key lsp-ui-mode-map (kbd "C-c l k") 'lsp-ui-doc-show)
   (define-key lsp-ui-mode-map (kbd "C-c l s") 'lsp-ui-doc-hide))
