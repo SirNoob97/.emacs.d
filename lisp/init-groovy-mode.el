e1895a77fc437ef3165c9a765164b185f8213708
SirNoob97 <martinald467@gmail.com>
2022-06-25 20:55:15 +0200

    based on the docs this is unnecessary

--- lisp/init-groovy-mode.el
+++ lisp/init-groovy-mode.el
@@ -9,12 +9,6 @@
 (add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
 (add-to-list 'auto-mode-alist '("\.gradle$" . groovy-mode))
 (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
- 
-;;; make Groovy mode electric by default.
-(add-hook 'groovy-mode-hook
-          '(lambda ()
-             (require 'groovy-electric)
-             (groovy-electric-mode)))
 
 (provide 'init-groovy-mode)
 
