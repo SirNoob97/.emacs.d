131537fefc7d6e6b1106656dff8173bf0cc10830
SirNoob97 <martinald467@gmail.com>
2022-06-30 16:14:38 +0200

    disable angular lsp due to some bugs

--- lisp/init-typescript.el
+++ lisp/init-typescript.el
@@ -12,6 +12,8 @@
   (add-hook 'typescript-mode-hook 'lsp)
   (setq typescript-indent-level 2))
 
+(setq lsp-clients-angular-node-get-prefix-command 'nil)
+
 (provide 'init-typescript)
 
 ;;; init-typescript.el ends here
