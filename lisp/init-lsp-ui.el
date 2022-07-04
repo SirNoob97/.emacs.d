7d93856e48999e1651290d429c871b3ebc40ec02
SirNoob97 <martinald467@gmail.com>
2022-07-04 15:22:09 +0200

    reorder and set doc position to at-point

--- lisp/init-lsp-ui.el
+++ lisp/init-lsp-ui.el
@@ -13,14 +13,14 @@
          ([remap xref-find-references] . lsp-ui-peek-find-references))
   :init
   (setq lsp-ui-peek-always-show t)
+  (setq	lsp-ui-doc-position 'at-point)
   (setq lsp-ui-doc-delay 1.5)
   (setq	lsp-ui-doc-include-signature t)
-  (setq	lsp-ui-doc-position 'bottom)
   (setq	lsp-ui-doc-max-width 100)
-  (setq lsp-ui-sideline-ignore-duplicate t)
-  (setq lsp-ui-sideline-show-diagnostics t)
   (setq	lsp-ui-doc-show-with-cursor nil)
   (setq	lsp-ui-doc-show-with-mouse nil)
+  (setq lsp-ui-sideline-ignore-duplicate t)
+  (setq lsp-ui-sideline-show-diagnostics t)
   (setq lsp-ui-sideline-show-code-actions nil)
   :config
   (define-key lsp-ui-mode-map (kbd "C-c l k") 'lsp-ui-doc-show)
